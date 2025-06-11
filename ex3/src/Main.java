9ee01387bc4c613a68fe144e65abfcfc5be77151
fezin <2411550401@fema.edu.br>
2025-06-10 21:08:01 -0300

    finish third exercise

--- ex3/src/Main.java
+++ ex3/src/Main.java
@@ -1,10 +1,26 @@
 package src;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args) {
+        Scanner prompt = new Scanner(System.in);
 
-        
+        System.out.println("Informe o primeiro valor: ");
+        int num1 = prompt.nextInt();
+
+        System.out.println("Informe o segundo valor: ");
+        int num2 = prompt.nextInt();
+
+        System.out.println("Informe o terceiro valor: ");
+        int num3 = prompt.nextInt();
+
+        int menor = num1;
+
+        if(num2<menor) menor = num2;
+        if(num3<menor) menor = num3;
+
+        System.out.println("MENOR = "+menor);
 
     }
 }
